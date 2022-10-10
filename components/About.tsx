85c0d5047dae81dbcfe619ba14750f489203dc20
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-10 18:05:15 +0300

    about component update

--- components/About.tsx
+++ components/About.tsx
@@ -5,8 +5,8 @@ type Props = {}
 
 export default function About({}: Props) {
   return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row justify-evenly mx-auto items-center'>
-      <h3 className='absolute top-24 uppercase tracking-[px] text-gray-500 text-2xl'>
+    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row justify-evenly mx-auto items-center max-w-7xl px-10'>
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         About
       </h3>
       <motion.img
@@ -20,7 +20,7 @@ export default function About({}: Props) {
         }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
-        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px]'
+        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] ml-3.5'
       />
       <motion.div
         initial={{
