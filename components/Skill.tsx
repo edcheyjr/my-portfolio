2386f4a8ff902e3ae673e9284febf8bd62793d2c
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 20:42:23 +0300

    🤩connected skills page to sanity cms

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Skill } from '../typings'
+import { urlFor } from '../lib/sanity'
+import Image from 'next/image'
+
 type Props = {
   directionLeft?: boolean
+  skill: Skill
 }
 
-function Skill({ directionLeft }: Props) {
+function Skill({ directionLeft, skill }: Props) {
   return (
     <motion.div
       initial={{
@@ -15,19 +20,24 @@ function Skill({ directionLeft }: Props) {
       whileInView={{ opacity: 1, x: 0 }}
       viewport={{ once: true }}
       className='group relative flex cursor-pointer'
+      title={skill?.title}
     >
-      <div className='rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28  xl:w-32 xl:h-32 filter group-hover:grayscale duration-300 ease-in-out items-center justify-center flex'>
-        <img
-          alt='React js'
-          src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
-          className='w-3/4'
+      <div className='rounded-full border border-gray-500 object-cover w-20 h-20 md:w-28 md:h-28  xl:w-32 xl:h-32 filter group-hover:grayscale duration-300 ease-in-out items-center justify-center flex relative'>
+        <Image
+          alt={skill.title}
+          src={
+            urlFor(skill?.image).url() ||
+            'https://cdn.sanity.io/images/e2f024ld/production/cfac7a956697f48ee9169dce6b8229f38de72472-294x345.svg'
+          }
+          width={56}
+          height={56}
         />
       </div>
 
       <div className='bg-primary absolute rounded-full w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 items-center flex justify-center opacity-0 hover:opacity-80 transition-opacity duration-200 ease-in-out'>
         <div className='bg-[#242424] rounded-full w-4/5 h-4/5 items-center flex justify-center '>
           <p className='text-base xl:text-xl font-bold text-primary opacity-full'>
-            100%
+            {skill.progress}
           </p>
         </div>
       </div>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,37 +1,32 @@
 import { motion } from 'framer-motion'
 import React from 'react'
+import { Skill as SkillType } from '../typings'
 import SectionTitle from './SectionTitle'
 import Skill from './Skill'
 
-type Props = {}
+type Props = {
+  skills: SkillType[]
+}
 
-function Skills({}: Props) {
+function Skills({ skills }: Props) {
   return (
-    <motion.div className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-0 items-center'>
+    <motion.div
+      className='flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] px-4 xl:px-10 min-h-screen items-center justify-end
+    xl:space-y-0 mx-0 '
+    >
       <SectionTitle> Skills </SectionTitle>
 
-      <h3 className='text-center absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
-        Over the years i have acquired various tech skills. Hover over them for
-        current profiecency{' '}
+      <h3 className='text-center absolute top-36 translate-x-1/2 right-1/2 transform uppercase tracking-widest md:tracking-[3px] text-gray-500 text-xs md:text-sm'>
+        Over the years i have acquired various tech skills.{' '}
+        <strong>Hover over them for current proficiency</strong>{' '}
       </h3>
-      <div className='pt-24 grid grid-cols-4 xl:grid-cols-5 gap-2'>
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
-        <Skill />
+      <div className='relative pt-36 grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 gap-2 mx-auto my-auto'>
+        {skills.slice(0, skills.length * 0.5).map((skill) => (
+          <Skill key={skill._id} skill={skill} directionLeft />
+        ))}
+        {skills.slice(skills.length * 0.5, skills.length).map((skill) => (
+          <Skill key={skill._id} skill={skill} />
+        ))}
       </div>
     </motion.div>
   )
