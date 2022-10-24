305b5811815dd8e0c72c922fd3da534448332b58
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 20:34:02 +0300

    update styles

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -4,7 +4,7 @@ type Props = { children: React.ReactNode }
 
 const SectionTitle = (props: Props) => {
   return (
-    <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl md:flex-row'>
+    <h3 className='absolute top-24 right-1/2  transform translate-x-1/2 uppercase tracking-[8px] lg:tracking-[20px] text-gray-500 text-xl md:text-2xl md:flex-row'>
       {props.children}
     </h3>
   )
