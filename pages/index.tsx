149a1c3e31c79477c55ad623075cca40fc673c1c
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 20:44:39 +0300

    connecting frontend to backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
+import React, { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 
@@ -12,12 +13,43 @@ import Projects from '../components/Projects'
 import Contact from '../components/Contact'
 import Link from 'next/link'
 import ImageSrc from '../public/image.jpg'
+import { Experience, PageInfo, Project, Skill, Social } from '../typings'
+import { fetchPageInfo } from '../utils/fetchPageInfo'
+import { fetchSkills } from '../utils/fetchSkills'
+import { fetchExperience } from '../utils/fetchExperience'
+import { fetchSocials } from '../utils/fetchSocials'
+import { fetchProjects } from '../utils/fetchProjects'
 
 // possible text color text-[rgb(128,128,128)]
 
-const Home: NextPage = () => {
+type Props = {
+  pageInfo: PageInfo
+  experience: Experience[]
+  skills: Skill[]
+  projects: Project[]
+  socials: Social[]
+}
+
+const Home = ({ pageInfo, skills, experience, projects, socials }: Props) => {
+  // const [isShowBackTopBtn, setShowBackToTop] = React.useState<boolean>(false)
+  // const stickyHeader = () => {
+  //   console.log(
+  //     'pageYOffset',
+  //     window.pageYOffset,
+  //     'window inner width',
+  //     window.innerWidth
+  //   )
+  //   console.log('width', window.innerWidth)
+  //   window.pageYOffset > window.innerWidth
+  //     ? setShowBackToTop(true)
+  //     : setShowBackToTop(false)
+  // }
+
   return (
-    <div className='z-0 bg-[rgb(36,36,36)] text-zinc-300 h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth transition-all delay-300 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary '>
+    <div
+      // onScroll={stickyHeader}
+      className='z-0 bg-[rgb(36,36,36)] text-zinc-300 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll scroll-smooth transition-all delay-300 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-primary '
+    >
       <Head>
         <title>Welcome to Ed&apos;s World</title>
         <meta
@@ -27,43 +59,44 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='mx-auto py-4'>
-        <Header />
+        <Header socials={socials} />
 
         <section id='hero' className='snap-center'>
-          <Hero />
+          <Hero pageInfo={pageInfo} />
         </section>
 
-        <section id='about' className='snap-center'>
-          <About />
+        <section id='about' className='snap-end'>
+          <About pageInfo={pageInfo} />
         </section>
 
         <section id='experience' className='snap-center'>
-          <WorkExperience />
+          <WorkExperience experiences={experience} />
         </section>
 
         <section id='skills' className='snap-start'>
-          <Skills />
+          <Skills skills={skills} />
         </section>
 
         <section id='projects' className='snap-start'>
-          <Projects />
+          <Projects projects={projects} />
         </section>
 
         <section id='contact' className='snap-start'>
-          <Contact />
+          <Contact pageInfo={pageInfo} />
         </section>
+
         <Link href={'#hero'}>
-          <footer className='sticky z-50 bottom-5 w-full cursor-pointer flex justify-center'>
+          <footer className='hidden lg:flex sticky z-50 bottom-5 w-full cursor-pointer justify-center'>
             <div
               title='go back to top'
-              className='relative h-10 w-10 rounded-full grayscale hover:grayscale-0 shadow-md hover:shadow hover:animate-bounce'
+              className='flex before:relative h-10 w-10 rounded-full grayscale hover:grayscale-0 shadow-md hover:shadow hover:animate-bounce'
             >
               <Image
                 src={ImageSrc}
                 alt='go back to top image button'
                 layout='fill'
                 objectFit='cover'
-                className='rounded-full'
+                className='rounded-full '
               />
             </div>
           </footer>
@@ -74,3 +107,21 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo = await fetchPageInfo()
+  const skills = await fetchSkills()
+  const experience = await fetchExperience()
+  const projects = await fetchProjects()
+  const socials = await fetchSocials()
+  return {
+    props: {
+      pageInfo,
+      skills,
+      experience,
+      projects,
+      socials,
+    },
+    revalidate: 10,
+  }
+}
