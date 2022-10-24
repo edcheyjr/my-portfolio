54b8673702b6622c9c0133e6dcaa848120c047df
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 21:48:06 +0300

    🈸 experience api

---
+++ pages/api/getExperience.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Experience } from '../../typings'
+
+import { groq } from 'next-sanity'
+import { getClient } from '../../lib/sanity.server'
+
+const query = groq`
+*[_type=="experience"]{
+ ...,
+ technologies[]->
+}
+`
+type Data = {
+  experience: Experience[]
+}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const experience: Experience[] = await getClient(false).fetch(query)
+  res.status(200).json({ experience })
+}
