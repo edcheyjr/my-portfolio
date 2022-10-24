5ec2361b015e8935a8fa492259f9be61ec2fd4ca
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 18:09:27 +0300

    🤩connected about page to sanity cms

--- components/About.tsx
+++ components/About.tsx
@@ -1,25 +1,33 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import SectionTitle from './SectionTitle'
+import { PageInfo } from '../typings'
+import { urlFor } from '../lib/sanity'
 
-type Props = {}
+type Props = {
+  pageInfo: PageInfo
+}
 
-export default function About({}: Props) {
+export default function About({ pageInfo }: Props) {
   return (
-    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row justify-evenly mx-auto items-center max-w-7xl px-10'>
+    <div className='h-screen flex relative flex-col text-center md:text-left md:flex-row justify-end md:justify-evenly pb-5 sm:pb-20 md:pb-0 mx-auto items-center max-w-7xl px-10'>
       <SectionTitle>About</SectionTitle>
       <motion.img
         initial={{
           x: -200,
           opacity: 0,
         }}
-        src='https://images.unsplash.com/photo-1665153856958-d1dadc34c523?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        alt={pageInfo?.name || 'No image'}
+        src={
+          urlFor(pageInfo?.image).url() ||
+          'https://images.unsplash.com/photo-1665153856958-d1dadc34c523?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+        }
         transition={{
           duration: 1.2,
         }}
         whileInView={{ x: 0, opacity: 1 }}
         viewport={{ once: true }}
-        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[500px] xl:h-[600px] ml-3.5'
+        className=' md:mb-0 flex-shrink-0 w-[12em] h-[12em] sm:w-56 sm:h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[450px] xl:h-[550px] ml-3.5'
       />
       <motion.div
         initial={{
@@ -33,7 +41,7 @@ export default function About({}: Props) {
         }}
         className='space-y-10 px-0 md:px-10'
       >
-        <h4 className='text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#f7abba]/70 via-gray-100 to-[#f7abba]/70'>
+        <h4 className='text-2xl md:text-4xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-[#f7abba]/70 via-gray-100 to-[#f7abba]/70'>
           A{' '}
           <span className='underline underline-offset-4 decoration-primary decoration-2'>
             little
@@ -41,18 +49,8 @@ export default function About({}: Props) {
           about me
         </h4>
         <p className='text-sm sm:text-base'>
-          Hola, I&lsquo;m Edwin Chebii😀. You might also know me as Ed Chey. I
-          will work with you on whatever exciting project you have in mind. A
-          little about me... I am a passionate full-stack software engineer
-          currently with 3 years of experience. I have worked with startups and
-          large corporate businesses to help scale and grow their businesses. I
-          also work as a freelancer building software products specially crafted
-          to meet individual needs. While not working, you will often find me
-          building new cool web apps or learning some new technology. If not
-          that, playing or developing games🎮 . As a strategic partner 🤝🏾, I
-          will help you digitally transform to improve your products, speed up
-          service delivery, increase operational efficiency, and expand to new
-          markets. Contact if interested.
+          {pageInfo?.backgroundInformation ||
+            'No background info at the moment'}
         </p>
       </motion.div>
     </div>
