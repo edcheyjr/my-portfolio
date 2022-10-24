fca7f7bac4b14ea5b408f966e99f39284c8c167b
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 21:48:46 +0300

    🈸 projects api

---
+++ pages/api/getProjects.ts
@@ -0,0 +1,23 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import type { Project } from '../../typings'
+
+import { groq } from 'next-sanity'
+import { sanityClient } from '../../lib/sanity.server'
+
+const query = groq`
+*[_type=="project"]{
+ ...,
+ technologies[]->
+}
+`
+type Data = {
+  project: Project[]
+}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const project: Project[] = await sanityClient.fetch(query)
+  res.status(200).json({ project })
+}
