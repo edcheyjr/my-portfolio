690450ff862a1f8f2b22fc37b1fb904490e5eebf
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 18:12:36 +0300

    🤩connected experience page to sanity cms

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-import Image from 'next/image'
 import Link from 'next/link'
+import { Experience } from '../typings'
+import { urlFor } from '../lib/sanity'
+import Technology from './Technology'
 
-type Props = {}
+type Props = {
+  experience: Experience
+}
 
-const ExperienceCard = (props: Props) => {
+const ExperienceCard = ({ experience }: Props) => {
   return (
-    <article className='flex flex-shrink-0 rounded-lg items-center space-y-7 flex-col w-[500px] md:w-[580px] py-10 bg-[#292929] opacity-40 hover:opacity-100 transition-opacity cursor-pointer duraction-200 snap-center'>
+    <article className='flex flex-shrink-0 rounded-lg items-center space-y-7 flex-col w-fit py-4 lg:py-8 bg-[#292929] opacity-40 hover:opacity-100 transition-opacity cursor-pointer duraction-200 snap-center'>
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true }}
-        className='rounded-full w-32 h-32 md:w-[150px] md:h-[150px] object-cover object-center'
-        src='/avatar.gif'
-        alt='my avatar'
+        className='rounded-full w-32 h-32 md:w-[100px] md:h-[100px] object-cover object-center'
+        src={urlFor(experience.companyImage).url()}
+        alt={experience.company || 'No company name'}
       />
       <motion.div
         initial={{
@@ -29,70 +33,29 @@ const ExperienceCard = (props: Props) => {
         }}
         className='px-0 md:px-10'
       >
-        <h4 className='text-3xl xl:text-4xl font-light capitalize'>
-          role in the company
+        <h4 className='text-center text-2xl lg:text-3xl font-light capitalize'>
+          {experience.jobTitle}
         </h4>
-        <p className='font-bold text2xl mt-1'>company</p>
-        <div className='flex space-x-2 my-2'>
+        <p className='text-center font-bold text2xl mt-1'>
+          {experience.company || 'No company name'}
+        </p>
+        <div className='justify-center flex space-x-2 my-2'>
           {/* tech stacks */}
-          <div className='relative h-10 w-10 rounded-full '>
-            <Image
-              alt='HTML5'
-              title='HTML5  Semantics'
-              className=''
-              layout='fill'
-              objectFit='contain'
-              src='https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'
-            />
-          </div>
-          <div className='relative h-10 w-10 rounded-full '>
-            <Image
-              alt='React'
-              title='React js'
-              layout='fill'
-              objectFit='contain'
-              src='https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
-            />
-          </div>
-          <div className='relative h-10 w-10 rounded-full '>
-            <Image
-              alt='Python'
-              title='python'
-              layout='fill'
-              objectFit='contain'
-              src='https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg'
-            />
-          </div>
-          <div className='relative h-10 w-10 rounded-full '>
-            <Image
-              alt='Laravel framework'
-              title='php laravel'
-              className=''
-              layout={'fill'}
-              objectFit={'contain'}
-              src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg'
-            />
-          </div>
-          <div className='relative h-10 w-10  rounded-full'>
-            <Image
-              alt='Php'
-              title='php'
-              layout='fill'
-              objectFit='contain'
-              src='https://upload.wikimedia.org/wikipedia/commons/3/31/Webysther_20160423_-_Elephpant.svg'
-            />
-          </div>
+          {experience?.technologies?.map((tech) => (
+            <Technology key={tech._id} tech={tech} />
+          ))}
         </div>
-        <div className='uppercase py-5 text-gray-300'>
-          started date.end date
+        <div className='text-center uppercase py-5 text-primary'>
+          {new Date(experience?.dateStarted).toDateString()} -{' '}
+          {experience?.isCurrentlyWorking
+            ? 'present'
+            : new Date(experience?.dateEnded).toDateString()}
         </div>
         {/* roles */}
-        <ul className='list-disc space-y-4 ml-5 md:text-lg'>
-          <li>Summary dolor sit amet consectetur adipisicing elit. </li>
-          <li>Summary dolor sit amet consectetur adipisicing elit. </li>
-          <li>Summary dolor sit amet consectetur adipisicing elit. </li>
-          <li>Summary dolor sit amet consectetur adipisicing elit. </li>
-          <li>Summary dolor sit amet consectetur adipisicing elit. </li>
+        <ul className='list-disc space-y-4 ml-5 md:text-lg px-7 h-64 overflow-x-scroll scrollbar-thumb-primary scrollbar-thin'>
+          {experience.points?.map((point, key) => (
+            <li key={key}>{point}</li>
+          ))}
         </ul>
       </motion.div>
     </article>
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import ExperienceCard from './ExperienceCard'
 import SectionTitle from './SectionTitle'
+import { Experience } from '../typings'
 
-type Props = {}
+type Props = {
+  experiences: Experience[]
+}
 
-const WorkExperience = (props: Props) => {
+const WorkExperience = ({ experiences }: Props) => {
   return (
-    <motion.div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-7xl justify-evenly mx-auto items-center px-10'>
+    <motion.div className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-7xl justify-end mx-auto items-center px-10'>
       <SectionTitle> Experience</SectionTitle>
 
-      <div className='relative w-full flex space-x-3 px-10 pb-10 overflow-x-scroll snap-x snap-mandatory  scroll-smooth  scrollbar-track-gray-400/20 scrollbar-thumb-primary scrollbar'>
+      <div className='relative w-screen flex space-x-4 overflow-x-scroll scroll-smooth scrollbar-track-gray-400/20 scrollbar-thumb-primary scrollbar mt-36 pb-10 px-3 lg:px-6'>
         {/* ExperienceCard */}
-        <ExperienceCard />
-        <ExperienceCard />
-        <ExperienceCard />
+
+        {experiences?.map((experience) => (
+          <ExperienceCard key={experience._id} experience={experience} />
+        ))}
         {/* ExperienceCard */}
       </div>
     </motion.div>
