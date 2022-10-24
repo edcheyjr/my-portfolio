ee0f6e3ff02a55ab7541674a83d5b96b9df13c18
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-10-24 18:00:40 +0300

    🕸canged allowed image domains�

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    EMAILJS_PUBLIC_KEY: 'o8pjtF-nSdq9CShme',
-    EMAILJS_SERVICE_ID: 'service_94xvoll',
-    EMAILJS_TEMPLATE_ID: 'template_4m1g1vb',
-    EMAILJS_reCAPTCHA_KEY: 'ed bot talking',
-    EMAILJS_TRACKING_ID: 'McQfTjWmZq4t7w!',
-  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['upload.wikimedia.org', 'commons.wikimedia.org', 'github.com'],
+    domains: ['cdn.sanity.io'],
   },
 }
 
