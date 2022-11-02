0832e940f618c9daf8f016a1a96a0b6a0e88ec40
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-02 17:40:21 +0300

    😁added tilt animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,27 @@ module.exports = {
     './pages/**/**/*.{js,ts,jsx,tsx}',
     './components/**/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: false, // media or class
   theme: {
     extend: {
       colors: {
         primary: '#F7ABBA',
+      },
+      animation: {
+        tilt: 'tilt 10s infinite linear',
+      },
+      keyframes: {
+        tilt: {
+          '0%, 50%, 100%': {
+            transform: 'rotate(0deg)',
+          },
+          '25%': {
+            transform: 'rotate(1deg)',
+          },
+          '75%': {
+            transform: 'rotate(-1deg)',
+          },
+        },
       },
     },
   },
