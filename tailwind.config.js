f6bda7f72ebb35113246db2efc56f280bcfcc9c0
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2022-11-22 18:57:48 +0300

    tailwind dark mode removed the class entirely 🌑

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     './pages/**/**/*.{js,ts,jsx,tsx}',
     './components/**/**/*.{js,ts,jsx,tsx}',
   ],
-  darkMode: false, // media or class
+  // darkMode: false, // media or class only use it when using class
   theme: {
     extend: {
       colors: {
