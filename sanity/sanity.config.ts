09c249cb9f895b3b1eb98c1ed826d2ac0ba3a097
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-22 14:03:37 +0300

    #26-📚 sanity update v2 to v3

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,48 @@
+import { deskTool } from 'sanity/desk'
+import { defineConfig } from 'sanity'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas/index'
+import deskStructure from './deskStructure'
+import { Logo } from './plugins/my-logo/Logo'
+
+export default defineConfig({
+  name: 'my-portfolio',
+  projectId: 'e2f024ld',
+  dataset: 'production',
+  plugins: [deskTool({ structure: deskStructure }), visionTool()],
+  schema: {
+    types: schemaTypes,
+  },
+  tools: (prev) => {
+    // 👇 Uses environment variables set by Vite in development mode
+    if (process.env.DEV) {
+      return prev
+    }
+    return prev.filter((tool) => tool.name !== 'vision')
+  },
+  studio: {
+    components: {
+      // custom components under here
+      logo: Logo,
+    },
+  },
+  // might need to include this or not
+  // document: {
+  //   newDocumentOptions: (prev, { creationContext }) => {
+  //     if (creationContext.type === 'global') {
+  //       return prev.filter(
+  //         (templateItem) => templateItem.templateId != 'settings'
+  //       )
+  //     }
+  //     return prev
+  //   },
+  //   actions: (prev, { schemaType }) => {
+  //     if (schemaType === 'settings') {
+  //       return prev.filter(
+  //         ({ action }) => !['unpublish', 'delete', 'duplicate'].includes(action)
+  //       )
+  //     }
+  //     return prev
+  //   },
+  // },
+})
---
+++ sanity/schemas/project.ts
@@ -0,0 +1,38 @@
+import { defineType, defineField } from 'sanity'
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      description: 'Title of the project',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'summary',
+      title: 'Summary',
+      type: 'text',
+    }),
+    defineField({
+      name: 'technologies',
+      title: 'Technologies',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'skill' } }],
+    }),
+    defineField({
+      name: 'linkTobuild',
+      title: 'LinkTobuild',
+      type: 'url',
+    }),
+  ],
+})
