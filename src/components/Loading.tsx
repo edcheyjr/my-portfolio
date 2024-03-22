cefc5fb7de549c8fea4ea0d33804d48047b7cb95
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-03-22 13:09:26 +0300

    [ui]:loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -5,7 +5,8 @@ type Props = {}
 
 const Loading = (props: Props) => {
   return (
-    <div className='min-h-screen w-full flex justify-center items-center'>
+    <div className='h-full w-full flex flx-col justify-center items-center space-y-2'>
+      <p className=' text-sm lg:text-lg font-bold'>Loading info...</p>{' '}
       <IcLoader className='animate-spin fill-current text-primary size-10' />
     </div>
   )
