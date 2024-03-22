1c23eef62b1d3aa12a5b7bb7d6940298489b092e
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2024-03-22 12:22:52 +0300

    [ui]:💻loading ui

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import IcLoader from '../svg/icons/ic-loader'
 
 type Props = {}
 
 const Loading = (props: Props) => {
-  return <div>Loading...</div>
+  return (
+    <div className='min-h-screen mx-auto my-auto'>
+      <IcLoader className='animate-spin fill-current text-primary size-10' />
+    </div>
+  )
 }
 
 export default Loading
